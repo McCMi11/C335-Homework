f482afa81010f8ab323434157ffd550be23bc9f9
Michael McCann <mimccann@indiana.edu>
2017-01-12 18:17:27 -0500

    Update Lab 1 Added comments to code in hello.c

--- Lab1/hello.c
+++ Lab1/hello.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
+/*
+  This program will print hello world
+  over and over again every 3 seconds
+  also changes colors of terminal
+ */
 int main(){
-  while(1){
-    int randomnumber;
-    randomnumber = rand() % 10;
+  while(1){ // allows to run forever
+    int randomnumber; // used for storing a random number to dictate color
+
+    randomnumber = rand() % 10; // generates the number
+    
+    /*
+      %d is used as int placeholder
+      
+      \33[0;3%dm    --  changes font color
+      \033[4%dm     --  changes background color
+      \033[4;3%dm   --  changes underline color
+     */
     printf("\33[0;3%dm\033[4%dm\033[4;3%dmHello World\n", randomnumber, randomnumber + 1 % 10, randomnumber + 3 % 10);
-    sleep(3);
+    sleep(3); // Pauses program for 3 seconds
   }
 }
