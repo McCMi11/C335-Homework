8647aceaab928cc418b61392c60b26ac65605b97
Michael McCann <mimccann@indiana.edu>
2017-01-19 14:50:21 -0500

    Update Lab 1 Added the correct headers to hello.c Added the function header to main() in hello.c

--- Lab1/hello.c
+++ Lab1/hello.c
@@ -1,9 +1,35 @@
+/******************************************
+ *
+ *
+ *   File: hello.c
+ *   Author: Michael McCann
+ *   School: Indiana University
+ *   Assignment: Lab 1
+ *   Part of: Lab Assignments
+ *   Description: Prints Hello World every
+ *      3 seconds.  Also changes the colors
+ *      of the terminal to random colors
+ *   Date Created: 01/12/2017
+ *   Date Modified: 01/19/2017
+ *   Modified By: Michael McCann
+ *
+ *   Revision: Added the header and fixed
+ *     some commenting to match requirements
+ */
+
 #include <stdio.h>
 
-/*
-  This program will print hello world
-  over and over again every 3 seconds
-  also changes colors of terminal
+/*****************
+ *   main()
+ *
+ *   Input: N/A
+ *   Assumptions of input: N/A
+ *   Guarantees about output: N/A, never returns
+ *   Description: Will print "Hello World"
+ *       infinitely every 3 seconds, changing
+ *       the terminals colors each time too.
+ *
+ *
  */
 int main(){
   while(1){ // allows to run forever
@@ -12,11 +38,13 @@ int main(){
     randomnumber = rand() % 8; // generates the number
     
     /*
-      %d is used as int placeholder
-      
-      \33[0;3%dm    --  changes font color
-      \033[4%dm     --  changes background color
-      \033[4;3%dm   --  changes underline color
+     *  Prints "Hello World"
+     *
+     *  %d is used as int placeholder
+     * 
+     *  \33[0;3%dm    --  changes font color
+     *  \033[4%dm     --  changes background color
+     *  \033[4;3%dm   --  changes underline color
      */
     printf("\33[0;3%dm\033[4%dm\033[4;3%dmHello World\n", randomnumber, randomnumber + 1 % 8, randomnumber + 3 % 8);
     sleep(3); // Pauses program for 3 seconds
